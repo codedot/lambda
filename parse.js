d12f04113790868a8ab7909f59a46f8d1c302720
Anton Salikhmetov <anton.salikhmetov@gmail.com>
2015-07-30 23:39:21 +0300

    Compile a lambda term into configuration

--- parse.js
+++ parse.js
@@ -6,6 +6,7 @@ var src = fs.readFileSync(process.argv[2], "utf8");
 var dict = parser.parse(src);
 var macros = dict.macros;
 var term = dict.term;
+var lastwire = 0;
 
 var i;
 
@@ -58,37 +59,12 @@ function getfv(obj, fv)
 	}
 }
 
-function markfv(obj, bv)
-{
-	var node = obj.node;
-
-	if (!bv)
-		bv = {};
-
-	if ("atom" == node) {
-		if (obj.name in bv)
-			obj.free = false;
-		else
-			obj.free = true;
-	} else if ("abst" == node) {
-		bv[obj.var] = true;
-		markfv(obj.body, bv);
-		delete bv[obj.var];
-	} else if ("appl" == node) {
-		markfv(obj.left, bv);
-		markfv(obj.right, bv);
-	}
-}
-
 function obj2mlc(obj)
 {
 	var node = obj.node;
 
-	if ("atom" == node) {
-		var mark = obj.free ? "*" : "";
-
-		return obj.name + mark;
-	}
+	if ("atom" == node)
+		return obj.name;
 
 	if ("abst" == node) {
 		var body = obj2mlc(obj.body);
@@ -116,8 +92,167 @@ function obj2mlc(obj)
 	}
 }
 
-markfv(term);
-console.log(obj2mlc(term) + ", where:", getfv(term));
+function getwire()
+{
+	++lastwire;
+
+	return "w" + lastwire.toFixed(0);
+}
+
+function mktwins(left, right)
+{
+	var fvleft = getfv(left);
+	var fvright = getfv(right);
+	var shared = getcap(fvleft, fvright);
+	var atom;
+
+	for (atom in shared) {
+		var wleft = getwire();
+		var wright = getwire();
+
+		shared[atom] = {
+			left: wleft,
+			right: wright
+		};
+	}
+
+	return shared;
+}
+
+function psi(shared)
+{
+	var list = [];
+	var template = "%s = \\amb(%s, \share(%s, %s), %s)";
+	var atom;
+
+	for (atom in shared) {
+		var twins = shared[atom];
+		var wleft = twins.left;
+		var wright = twins.right;
+		var wire = getwire();
+		var eqn = template;
+
+		eqn = eqn.replace("%s", wleft);
+		eqn = eqn.replace("%s", wright);
+		eqn = eqn.replace("%s", atom);
+		eqn = eqn.replace("%s", wire);
+		eqn = eqn.replace("%s", wire);
+
+		list.push(eqn);
+	}
+
+	return list;
+}
+
+function gamma(obj, root)
+{
+	var list = [];
+	var node = obj.node;
+	var eqn = root + " = %s";
+
+	if ("atom" == node) {
+		var agent = "\\atom_{strdup(\"%s\")}";
+
+		if (obj.free)
+			agent = agent.replace("%s", obj.name);
+		else
+			agent = obj.name;
+
+		eqn = eqn.replace("%s", agent);
+		list.push(eqn);
+	} else if ("abst" == node) {
+		var wire = getwire();
+		var tree = "\\lambda(%s, %s)";
+		var body = obj.body;
+		var fv = getfv(body);
+		var id = obj.var;
+
+		if (id in fv)
+			agent = id;
+		else
+			agent = "\\erase";
+
+		tree = tree.replace("%s", agent);
+		tree = tree.replace("%s", wire);
+		eqn = eqn.replace("%s", tree);
+		list.push(eqn);
+
+		body = gamma(body, wire);
+		list = list.concat(body);
+	} else if ("appl" == node) {
+		var wleft = getwire();
+		var wright = getwire();
+		var agent = "\\apply(%s, %s)";
+		var left = obj.left;
+		var right = obj.right;
+		var shared = mktwins(left, right);
+
+		agent = agent.replace("%s", wleft);
+		agent = agent.replace("%s", wright);
+		eqn = eqn.replace("%s", agent);
+		list.push(eqn);
+
+		left = gamma(left, wleft);
+		right = gamma(right, wright);
+		shared = psi(shared);
+		list = list.concat(left, right, shared);
+	}
+
+	return list;
+}
+
+function alpha(obj, bv)
+{
+	var node = obj.node;
+
+	if (!bv)
+		bv = {};
+
+	if ("atom" == node) {
+		var name = obj.name;
+
+		if (name in bv) {
+			obj = {
+				node: "atom",
+				name: bv[name],
+				free: false
+			};
+		} else {
+			obj = {
+				node: "atom",
+				name: name,
+				free: true
+			};
+		}
+	} else if ("abst" == node) {
+		var id = obj.var;
+		var wire = getwire();
+		var body;
+
+		bv[id] = wire;
+		body = alpha(obj.body, bv);
+		delete bv[id];
+
+		obj = {
+			node: "abst",
+			var: wire,
+			body: body
+		};
+	} else if ("appl" == node) {
+		var left = alpha(obj.left, bv);
+		var right = alpha(obj.right, bv);
+
+		obj = {
+			node: "appl",
+			left: left,
+			right: right
+		};
+	}
+
+	return obj;
+}
+
+console.log(obj2mlc(term) + ", where:\n");
 
 for (i = 0; i < macros.length; i++) {
 	var macro = macros[i];
@@ -134,9 +269,14 @@ for (i = 0; i < macros.length; i++) {
 		right: def
 	};
 
-	markfv(def);
-	console.log(id + " = " + obj2mlc(def) + ";", getfv(def));
+	console.log(id + " = " + obj2mlc(def) + ";");
 }
 
-markfv(term);
-console.log(obj2mlc(term), getfv(term));
+term = alpha(term);
+console.log("\n" + obj2mlc(term));
+
+console.log("\n\\read_{strdup(\"%s\")}(\\print) = root;");
+
+eqns = gamma(term, "root");
+for (i = 0; i < eqns.length; i++)
+	console.log(eqns[i] + ";");
