c3d30d772d6065cda0a1946a879806febc221624
Anton Salikhmetov <anton.salikhmetov@gmail.com>
2015-08-14 14:20:13 +0300

    Determine signature of interaction system

--- interact.js
+++ interact.js
@@ -7,9 +7,47 @@ var system = parser.parse(src);
 var rules = system.rules;
 var conf = system.conf;
 
+var types = {
+	wire: true,
+	amb: true
+};
+
 var i;
 
-console.log({
-	nrules: rules.length,
-	neqns: conf.length
-});
+function gettypes(tree, dict)
+{
+	var node = tree.node;
+	var pax = tree.pax;
+	var i;
+
+	if (!dict)
+		dict = {};
+
+	if ("agent" == node.type)
+		dict[node.name] = true;
+
+	for (i = 0; i < pax.length; i++)
+		gettypes(pax[i], dict);
+
+	return dict;
+}
+
+for (i = 0; i < rules.length; i++) {
+	var rule = rules[i];
+	var left = rule.left;
+	var right = rule.right;
+
+	gettypes(left, types);
+	gettypes(right, types);
+}
+
+for (i = 0; i < conf.length; i++) {
+	var eqn = conf[i];
+	var left = eqn.left;
+	var right = eqn.right;
+
+	gettypes(left, types);
+	gettypes(right, types);
+}
+
+console.log(types);
