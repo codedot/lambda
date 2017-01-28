ed22051d439921f6bb7b5124fbde755219255621
Anton Salikhmetov <anton.salikhmetov@gmail.com>
2017-01-28 04:26:05 +0200

    Reduce other implementations

--- encoding/optimal/index.js
+++ encoding/optimal/index.js
@@ -1,151 +1,14 @@
 "use strict";
 
-var mlc = require("./lambda");
-var fs = require("fs");
+const generic = require("../generic");
+const fs = require("fs");
+const path = require("path");
 
-var parser = new mlc.Parser();
-var system = fs.readFileSync("template.txt", "utf8");
-var lastwire;
-
-function getcap(left, right)
-{
-	var dict = {};
-	var prop;
-
-	for (prop in left)
-		if (prop in right)
-			dict[prop] = left[prop];
-
-	return dict;
-}
-
-function merge(left, right)
-{
-	var dict = {};
-	var prop;
-
-	for (prop in left)
-		dict[prop] = left[prop];
-
-	for (prop in right)
-		dict[prop] = right[prop];
-
-	return dict;
-}
-
-function getfv(obj, fv)
-{
-	var node = obj.node;
-
-	if ("atom" == node) {
-		var fv = {};
-
-		if (!obj.free)
-			fv[obj.name] = true;
-
-		return fv;
-	} else if ("abst" == node) {
-		var fv = getfv(obj.body);
-
-		delete fv[obj.var];
-		return fv;
-	} else if ("appl" == node) {
-		var left = getfv(obj.left);
-		var right = getfv(obj.right);
-		var fv = merge(left, right);
-
-		return fv;
-	}
-}
-
-function obj2mlc(obj)
-{
-	var node = obj.node;
-
-	if ("atom" == node)
-		return obj.name;
-
-	if ("abst" == node) {
-		var body = obj2mlc(obj.body);
-		var sep;
-
-		if ("abst" == obj.body.node)
-			sep = ", ";
-		else
-			sep = ": ";
-
-		return obj.var + sep + body;
-	}
-
-	if ("appl" == node) {
-		var left = obj2mlc(obj.left);
-		var right = obj2mlc(obj.right);
-
-		if ("abst" == obj.left.node)
-			left = "(" + left + ")";
-
-		if ("abst" == obj.right.node)
-			right = "(" + right + ")";
-
-		if ("appl" == obj.right.node)
-			right = "(" + right + ")";
-
-		return left + " " + right;
-	}
-
-	return "[ ]";
-}
-
-function mkwire()
-{
-	++lastwire;
-	return "w" + lastwire.toFixed(0);
-}
-
-function subst(obj, shared, side)
-{
-	var node = obj.node;
-
-	if ("atom" == node) {
-		var name = obj.name;
-
-		if (name in shared) {
-			var entry = shared[name];
-
-			obj.name = entry[side];
-		}
-	} else if ("abst" == node) {
-		var body = obj.body;
-
-		subst(body, shared, side);
-	} else if ("appl" == node) {
-		subst(obj.left, shared, side);
-		subst(obj.right, shared, side);
-	}
-}
-
-function mktwins(left, right)
-{
-	var fvleft = getfv(left);
-	var fvright = getfv(right);
-	var shared = getcap(fvleft, fvright);
-	var atom;
-
-	for (atom in shared) {
-		var wleft = mkwire();
-		var wright = mkwire();
-
-		shared[atom] = {
-			left: wleft,
-			right: wright
-		};
-	}
-
-	subst(left, shared, "left");
-	subst(right, shared, "right");
-
-	return shared;
-}
+const system = fs.readFileSync(path.join(__dirname, "template.txt"), "utf8");
+const expand = generic.expand;
+const mkwire = generic.mkwire;
+const mktwins = generic.mktwins;
+const getfv = generic.getfv;
 
 function psi(shared)
 {
@@ -236,118 +99,16 @@ function gamma(obj, root)
 	return list;
 }
 
-function alpha(obj, bv, lvl)
+function encode(term)
 {
-	var node = obj.node;
+	let inconfig = [
+		"\\eval(\\read_{this.mkhole()}(\\print)) = root"
+	];
 
-	if (!bv)
-		bv = {};
+	gamma(expand(term), "root", inconfig);
+	inconfig = inconfig.join(";\n") + ";";
 
-	if (!lvl)
-		lvl = 0;
-
-	if ("atom" == node) {
-		var name = obj.name;
-
-		if (name in bv) {
-			var id = bv[name];
-
-			obj = {
-				node: "atom",
-				name: id.name,
-				index: lvl - id.lvl,
-				free: false
-			};
-		} else {
-			obj = {
-				node: "atom",
-				name: name,
-				free: true
-			};
-		}
-	} else if ("abst" == node) {
-		var id = obj.var;
-		var wire = mkwire();
-		var old = bv[id];
-		var body;
-
-		++lvl;
-		bv[id] = {
-			name: wire,
-			lvl: lvl
-		};
-		body = alpha(obj.body, bv, lvl);
-		delete bv[id];
-
-		if (old)
-			bv[id] = old;
-
-		obj = {
-			node: "abst",
-			var: wire,
-			body: body
-		};
-	} else if ("appl" == node) {
-		var left = alpha(obj.left, bv, lvl);
-		var right = alpha(obj.right, bv, lvl);
-
-		obj = {
-			node: "appl",
-			left: left,
-			right: right
-		};
-	}
-
-	return obj;
+	return system.replace("INCONFIG", inconfig);
 }
-
-function getconf(obj)
-{
-	var root = "\\eval(\\read_{this.mkhole()}(\\print))";
-	var conf;
-
-	lastwire = 0;
-
-	obj = alpha(obj);
-	conf = gamma(obj, "root", 0);
-	conf.push(root + " = root");
-	return conf;
-}
-
-function encode(mlc)
-{
-	var dict = parser.parse(mlc);
-	var macros = dict.macros;
-	var term = dict.term;
-	var inconfig = "";
-	var eqns, i;
-
-	for (i = 0; i < macros.length; i++) {
-		var macro = macros[i];
-		var id = macro.id;
-		var def = macro.def;
-
-		term = {
-			node: "appl",
-			left: {
-				node: "abst",
-				var: id,
-				body: term
-			},
-			right: def
-		};
-	}
-
-	encode.term = obj2mlc(term);
-
-	eqns = getconf(term);
-
-	for (i = 0; i < eqns.length; i++)
-		inconfig = inconfig.concat(eqns[i] + ";\n");
-
-	return system.replace("INCONFIG\n", inconfig);
-}
-
-encode.obj2mlc = obj2mlc;
 
 module.exports = encode;
