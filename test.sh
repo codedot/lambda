76dc5cc76da2c0e1af874910a3951872943e6649
Anton Salikhmetov <anton.salikhmetov@gmail.com>
2017-01-30 00:41:25 +0200

    Check nf in test.sh

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ run()
 {
 	node lambda -p samples/$1.mlc -a $2 >|$OUT 2>|$ERR
 
-	if [ $? -eq 0 ]; then
+	if [ $? -eq 0 -a "$(cat $OUT)" = "$3" ]; then
 		printf $CELLF "$(sed "$SED" $ERR)"
 	else
 		printf $CELLF "N/A"
@@ -19,11 +19,13 @@ run()
 compare()
 {
 	term=$1
-	printf $TERMF $term
-	shift
+	nf=$2
+	shift 2
 
-	for a; do
-		run $term $a
+	printf $TERMF $term
+
+	for algo; do
+		run $term $algo "$nf"
 	done
 
 	printf "\n"
@@ -31,8 +33,17 @@ compare()
 
 printf $HEADF SAMPLE ABSTRACT CLOSED NORMAL OPTIMAL TURNING
 
-compare test    abstract closed normal optimal turning
-compare exp     ABSTRACT closed NORMAL OPTIMAL TURNING
-compare asperti abstract closed normal optimal turning
-compare fact    abstract closed normal optimal turning
-compare larger  abstract closed normal OPTIMAL turning
+compare test "v1: v1 (v2: v2 v2) (v3: v3 v3)" \
+	abstract closed normal optimal turning
+
+compare exp "v1: v1" \
+	ABSTRACT closed NORMAL OPTIMAL TURNING
+
+compare asperti "v1, v2: v1 (v1 (v1 (v1 v2)))" \
+	abstract closed normal optimal turning
+
+compare fact "v1, v2: v1 (v1 (v1 v2))" \
+	abstract closed normal optimal turning
+
+compare larger "v1, v2: v1 (v1 (v1 (v1 (v1 v2))))" \
+	abstract closed normal OPTIMAL turning
