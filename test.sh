39836c74a3311048fea2a7ea4075d47388f53c84
Anton Salikhmetov <anton.salikhmetov@gmail.com>
2017-11-05 13:59:19 +0200

    Test (Fibo 16 Not T)

--- test.sh
+++ test.sh
@@ -34,16 +34,17 @@ compare()
 printf "$TERMF$CELLF$CELLF$CELLF\n" SAMPLE \
 	ABSTRACT CLOSED OPTIMAL
 
-compare counter abstract closed optimal
-compare w2eta   abstract closed optimal
-compare 1021    abstract closed optimal
-compare 22210i  abstract closed optimal
-compare 3222i   abstract closed optimal
-compare 1022i   abstract closed OPTIMAL
-compare 4222i   abstract closed OPTIMAL
-compare 222210i abstract closed OPTIMAL
-compare 2222101 abstract CLOSED OPTIMAL
-compare cfact4  abstract closed optimal
-compare yfact4  abstract closed optimal
-compare cfact5  abstract closed optimal
-compare yfact5  abstract closed OPTIMAL
+compare counter  abstract closed optimal
+compare w2eta    abstract closed optimal
+compare 1021     abstract closed optimal
+compare 22210i   abstract closed optimal
+compare 3222i    abstract closed optimal
+compare 1022i    abstract closed OPTIMAL
+compare 4222i    abstract closed OPTIMAL
+compare 222210i  abstract closed OPTIMAL
+compare 2222101  abstract CLOSED OPTIMAL
+compare cfact4   abstract closed optimal
+compare yfact4   abstract closed optimal
+compare fibo16nt abstract closed optimal
+compare cfact5   abstract closed optimal
+compare yfact5   abstract closed OPTIMAL
